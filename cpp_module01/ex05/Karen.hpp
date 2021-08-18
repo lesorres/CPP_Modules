ac2ad8ff859e647311d2dc983e5ea892ef43cb84
kmeeseek <beaverdi147@gmail.com>
2021-08-18 21:08:24 +0300

    m01ex05 final

--- cpp_module01/ex05/Karen.hpp
+++ cpp_module01/ex05/Karen.hpp
@@ -6,45 +6,28 @@
 /*   By: kmeeseek <kmeeseek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/17 18:57:11 by kmeeseek          #+#    #+#             */
-/*   Updated: 2021/08/18 19:54:23 by kmeeseek         ###   ########.fr       */
+/*   Updated: 2021/08/18 20:31:42 by kmeeseek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <string>
 # include <iostream>
 
-// typedef struct s_level
-// {
-// 	std::string level;
-// 	void (*Karen::method)(void);
-// }	t_level;
-
-
 class Karen
 {
 private:
 
-
-	// t_level _levels[4];
-	// void init_levels(void);
-	// void (Karen::*methods[5])(std::string const &target) = {&FragTrap::meleeAttack, &FragTrap::rangedAttack, &FragTrap::magicAttack, &FragTrap::sneakAttack, &FragTrap::panicAttack}; 
-
-public:
 	void debug(void);
 	void info(void);
 	void warning(void);
 	void error(void);
-	
 	std::string _input[4];
-	//typedef void (*method)(void);
-	//method methods[4];// = {&debug, &info, &warning, &error};
 	typedef void (Karen:: * PType)(void);
 	PType methods[4];
-
-	//void (Karen::*func[2])(void) = { &Karen::debug, &Karen::info };
-
 	void init_input_array(void);
 	void init_method_array(void);
+
+public:
 
 	void complain(std::string level);
 	Karen(void);
--- cpp_module01/ex05/main.cpp
+++ cpp_module01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kmeeseek <kmeeseek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/17 20:06:44 by kmeeseek          #+#    #+#             */
-/*   Updated: 2021/08/18 20:19:33 by kmeeseek         ###   ########.fr       */
+/*   Updated: 2021/08/18 21:07:17 by kmeeseek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,5 @@ int main ()
 	std::getline(std::cin, line);
 	karen.complain(line);
 
-	// std::cout << karen._input[0] << "\n";
-	// (karen.*karen.methods[3])();
 	return (0);
-
 }
