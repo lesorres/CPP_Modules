927a33af94a264aa2b18d113d33755a90aeb2e23
kmeeseek <beaverdi147@gmail.com>
2021-08-30 23:15:32 +0300

    done

--- cpp_module02/ex02/Fixed.cpp
+++ cpp_module02/ex02/Fixed.cpp
@@ -6,44 +6,46 @@
 /*   By: kmeeseek <kmeeseek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/19 22:13:42 by kmeeseek          #+#    #+#             */
-/*   Updated: 2021/08/29 22:35:54 by kmeeseek         ###   ########.fr       */
+/*   Updated: 2021/08/30 22:44:44 by kmeeseek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
+const int Fixed::_numOfFb = 8;
+
 Fixed::Fixed()
 {
-	std::cout << "Default constructor called" << std::endl;
+	// std::cout << "Default constructor called" << std::endl;
 	_fpVal = 0;
 }
 
 Fixed::Fixed(int i)
 {
-	std::cout << "Int constructor called" << std::endl;
+	// std::cout << "Int constructor called" << std::endl;
 	_fpVal = i <<_numOfFb;
 }
 
 Fixed::Fixed(float f)
 {
-	std::cout << "Float constructor called" << std::endl;
+	// std::cout << "Float constructor called" << std::endl;
 	_fpVal = roundf(f * (1 << _numOfFb));
 }
 
 Fixed::Fixed(Fixed const &src)
 {
-	std::cout << "Copy constructor called" << std::endl;
+	// std::cout << "Copy constructor called" << std::endl;
 	*this = src;
 }
 
 Fixed::~Fixed()
 {
-	std::cout << "Destructor called" << std::endl;
+	// std::cout << "Destructor called" << std::endl;
 }
 
 Fixed &Fixed::operator=(Fixed const &prm)
 {
-	std::cout << "Assignation operator called" << std::endl;
+	// std::cout << "Assignation operator called" << std::endl;
 	if (this == &prm)
 		return (*this);
 	this->setRawBits(prm.getRawBits());
@@ -76,6 +78,8 @@ int Fixed::toInt( void ) const
 	return(_fpVal >> _numOfFb);
 }
 
+/********** math operators **********/
+
 Fixed Fixed::operator*(Fixed const &num)
 {
 	Fixed ret;
@@ -90,4 +94,98 @@ Fixed Fixed::operator/(Fixed const &num)
 	return (ret);
 }
 
-const int Fixed::_numOfFb = 8;
+Fixed Fixed::operator+(Fixed const &num)
+{
+	Fixed ret;
+	ret._fpVal = this->_fpVal + num._fpVal;
+	return (ret);
+}
+
+Fixed Fixed::operator-(Fixed const &num)
+{
+	Fixed ret;
+	ret._fpVal = this->_fpVal - num._fpVal;
+	return (ret);
+}
+
+Fixed Fixed::operator++(int)
+{
+	Fixed tmp(*this);
+	this->_fpVal++;
+	return (tmp);
+}
+Fixed Fixed::operator++()
+{
+	this->_fpVal++;
+	return(*this);
+}
+Fixed Fixed::operator--(int)
+{
+	Fixed tmp(*this);
+	this->_fpVal--;
+	return (tmp);
+}
+Fixed Fixed::operator--()
+{
+	this->_fpVal--;
+	return(*this);
+}
+
+/********** logical operators **********/
+
+bool Fixed::operator>(Fixed const &num) const
+{
+	return (this->_fpVal > num._fpVal);
+}
+
+bool Fixed::operator<(Fixed const &num) const
+{
+	return (this->_fpVal < num._fpVal);
+}
+
+bool Fixed::operator>=(Fixed const &num) const
+{
+	return (this->_fpVal >= num._fpVal);
+}
+
+bool Fixed::operator<=(Fixed const &num) const
+{
+	return (this->_fpVal <= num._fpVal);
+}
+
+bool Fixed::operator==(Fixed const &num) const
+{
+	return (this->_fpVal == num._fpVal);
+}
+
+bool Fixed::operator!=(Fixed const &num) const
+{
+	return (this->_fpVal != num._fpVal);
+}
+
+/********** min & max **********/
+
+Fixed& Fixed::min( Fixed& a, Fixed& b )
+{
+	if (a > b)
+		return(b);
+	return(a);
+}
+Fixed& Fixed::max( Fixed& a, Fixed& b )
+{
+	if (a < b)
+		return(b);
+	return(a);
+}
+const Fixed& Fixed::min( const Fixed& a, const Fixed& b )
+{
+	if (a > b)
+		return(b);
+	return(a);
+}
+const Fixed& Fixed::max( const Fixed& a, const Fixed& b )
+{
+	if (a < b)
+		return(b);
+	return(a);
+}
--- cpp_module02/ex02/main.cpp
+++ cpp_module02/ex02/main.cpp
@@ -6,31 +6,50 @@
 /*   By: kmeeseek <kmeeseek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/19 22:06:27 by kmeeseek          #+#    #+#             */
-/*   Updated: 2021/08/29 22:39:22 by kmeeseek         ###   ########.fr       */
+/*   Updated: 2021/08/30 23:07:36 by kmeeseek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 
 int main( void )
 {
+	std::cout << "\ndefault tests:\n\n";
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
 	std::cout << a << std::endl;
-	// std::cout << ++a << std::endl;
+	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
-	// std::cout << a++ << std::endl;
+	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
 
 	std::cout << b << std::endl;
 
-	// std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
 
-	std::cout << std::setprecision(4);
-	Fixed const d( Fixed( 5.05f ) / Fixed( 2 ) );
-	std::cout << d << std::endl;
+	/********** my tests **********/
+	std::cout << "\nmy tests:\n\n";
+	std::cout << "a = " << a << std::endl;
+	std::cout << "b = " << b << std::endl << std::endl;
+
+	std::cout << "min (a, b): " << Fixed::min( a, b ) << std::endl;
+	Fixed const d( Fixed( 5.25f ) / Fixed( 2 ) );
+	std::cout << "5.25 / 2 = " << d << std::endl;
+	Fixed const z( Fixed( 5.25f ) + Fixed( 2 ) );
+	std::cout << "5.25 + 2 = " << z << std::endl;
+	Fixed const x( Fixed( 5 ) - Fixed( 2.5f ) );
+	std::cout << "5 - 2.5 = " << x << std::endl;
+	std::cout << "a == b = " << ((a == b) ? "true" : "false") << std::endl;
+	std::cout << "a != b = " << ((a != b) ? "true" : "false") << std::endl;
+	std::cout << "a >= b = " << ((a >= b) ? "true" : "false") << std::endl;
+	std::cout << "a <= b = " << ((a <= b) ? "true" : "false") << std::endl;
+	std::cout << "a-- = " << (a--) << std::endl;
+	std::cout << "a (decremented after previous operation) = " << a << std::endl;
+	std::cout << "--a = " << (--a) << std::endl;
+
 	return 0;
 }
