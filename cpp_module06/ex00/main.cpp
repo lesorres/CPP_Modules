f7ca64467fbc580914bd320116c26bd67b34ca39
kmeeseek <beaverdi147@gmail.com>
2021-09-14 21:27:09 +0300

    almost empty

---
+++ cpp_module06/ex00/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmeeseek <kmeeseek@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/14 19:11:22 by kmeeseek          #+#    #+#             */
+/*   Updated: 2021/09/14 19:37:03 by kmeeseek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main (int argc, char const **argv)
+{
+	if (argc = 2)
+	{
+		std::cout << "error: invalid number of arguments id given\n";
+		return (0);
+	}
+	
+
+}
